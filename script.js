49ae096bbda1c28ea2ebb9ae2a1fb78bf5970ee0
cvl <cintyavs@aolcom>
2023-02-17 20:41:57 -0500

    Added some elements to the compute function

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function compute()
 {
     p = document.getElementById("principal").value;
-    
+    r = document.getElementById("rate").value
+    y = document.getElementById("years").value
 }
         
